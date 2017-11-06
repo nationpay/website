20c87ab7f31b191bf5be14a0d135b01d23f80c69
Alex Oviedo <alexfabi1@gmail.com>
2017-11-07 00:15:25 +0100

    some changes and added social sharing info

--- src/scenes/Home/components/Advantages/index.js
+++ src/scenes/Home/components/Advantages/index.js
@@ -25,11 +25,11 @@ class Advantages extends Component {
                     <div className="row advantages">
                         <div className="col-md-10 col-md-offset-1">
                             <div className="row">
-                                <div className="col-md-4 item">
+                                <div className="col-md-4 item wow bounceInRight wowed animated" data-wow-duration="0.5s" data-wow-delay="0.25s">
                                     <div className="contain">
                                         <div className="img">
                                             <div className="circle">
-                                                <i className="fa fa-line-chart" aria-hidden="true"></i>
+                                                <i className="fa fa-lock" aria-hidden="true"></i>
                                             </div>
                                         </div>
                                         <h3>Pin Access</h3>
@@ -37,55 +37,55 @@ class Advantages extends Component {
                                     </div>
 
                                 </div>
-                                <div className="col-md-4 item">
+                                <div className="col-md-4 item wow bounceInRight wowed animated" data-wow-duration="0.5s" data-wow-delay="0.75s">
                                     <div className="contain">
                                         <div className="img">
                                             <div className="circle">
-                                                <i className="fa fa-line-chart" aria-hidden="true"></i>
+                                                <i className="fa fa-money" aria-hidden="true"></i>
                                             </div>
                                         </div>
                                         <h3>Cash for Tokens at the ATM</h3>
                                         <p>Just look for the NationPay logo at your bank ATM.  Getting cash is easy with QR scan.  Simply send tokens to the ATM and get cash.</p>
                                     </div>
                                 </div>
-                                <div className="col-md-4 item">
+                                <div className="col-md-4 item wow bounceInRight wowed animated" data-wow-duration="0.5s" data-wow-delay="1.25s">
                                     <div className="contain">
                                         <div className="img">
                                             <div className="circle">
-                                                <i className="fa fa-line-chart" aria-hidden="true"></i>
+                                                <i className="fa fa-thumbs-o-up" aria-hidden="true"></i>
                                             </div>
                                         </div>
                                         <h3>Cash out at Foreign ATMs</h3>
                                         <p>If the ATM is not a client of the bank whose tokens you have, simply trade on the NationPay marketplace for tokens of the bank whose symbol is on the ATM.  It's that simple.</p>
                                     </div>
                                 </div>
-                                <div className="col-md-4 item">
+                                <div className="col-md-4 item wow bounceInRight wowed animated" data-wow-duration="0.5s" data-wow-delay="1.75s">
                                     <div className="contain">
                                         <div className="img">
                                             <div className="circle">
-                                                <i className="fa fa-line-chart" aria-hidden="true"></i>
+                                                <i className="fa fa-times" aria-hidden="true"></i>
                                             </div>
                                         </div>
                                         <h3>No Physical Cards</h3>
                                         <p>No more credit or debit cards to carry.</p>
                                     </div>
                                 </div>
-                                <div className="col-md-4 item">
+                                <div className="col-md-4 item wow bounceInRight wowed animated" data-wow-duration="0.5s" data-wow-delay="2.25s">
                                     <div className="contain">
                                         <div className="img">
                                             <div className="circle">
-                                                <i className="fa fa-line-chart" aria-hidden="true"></i>
+                                                <i className="fa fa-exchange" aria-hidden="true"></i>
                                             </div>
                                         </div>
                                         <h3>Tourism</h3>
                                         <p>Trade for tokens at a bank in a foreign country you are going to visit, then go to the ATM accepting those tokens to get cash in that currency.</p>
                                     </div>
                                 </div>
-                                <div className="col-md-4 item">
+                                <div className="col-md-4 item wow bounceInRight wowed animated" data-wow-duration="0.5s" data-wow-delay="2.75s">
                                     <div className="contain">
                                         <div className="img">
                                             <div className="circle">
-                                                <i className="fa fa-line-chart" aria-hidden="true"></i>
+                                                <i className="fa fa-rocket" aria-hidden="true"></i>
                                             </div>
                                         </div>
                                         <h3>Remittance</h3>
